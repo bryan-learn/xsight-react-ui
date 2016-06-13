ec0ea6a5af224aa1ac69df09681877ba5d24de12
Bryan Learn <blearn@psc.edu>
2016-06-13 10:38:11 -0400

    add references to forceGraphComponent

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var updown = require('./bytesChartComponent.js');
 var line = require('./component.js');
+var forceGraph = require('./forceGraphComponent.js');
 
 //ReactDOM.render(<updown.MyChart url="api/content" pollInterval={5000}/>, document.getElementById('xs-traffic')); //document.body
 ReactDOM.render(<line.MyChart url="api/queryByFlow/DataOctetsOut/0598149c05b14c8cd780b21567d2aadcd0b78810fdb3726555e5aa998fd65c3e" pollInterval={5000}/>, document.getElementById('xs-chart')); //document.body
