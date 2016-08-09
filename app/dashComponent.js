d70fac20651820da50e2e234308a8194df53cf5c
blearn <blearn@psc.edu>
2016-08-09 16:11:30 -0400

    added reactd3 components for traffic view (pie chart) and quality view (waveform)

--- app/dashComponent.js
+++ app/dashComponent.js
@@ -1,6 +1,7 @@
 var exports = module.exports = {};
 var $ = require('jquery');
 var React = require('react');
+var ReactD3 = require('react-d3-components');
 var forceGraph = require('./forceGraph.js');
 var logger = require('./logger.js');
 var d3 = require('d3');
@@ -35,11 +36,47 @@ var SelectedSourceTable = React.createClass({
 });
 
 var TrafficView = React.createClass({
-    
+    render: function(){
+        var data = {
+            label: 'Traffic',
+            values: [{x: 'DTN1', y: 68},{x: 'DTN2', y: 27},{x: 'DTN3', y: 10}]
+        };
+
+        return (<ReactD3.PieChart
+            data={data}
+            width={600}
+            height={400}
+            margin={{top: 10, bottom: 10, left: 100, right: 100}}
+            sort={null}
+            />
+        );
+    }
 });
 
 var QualityView = React.createClass({
-    
+    render: function(){
+        var data = [{
+            label: 'Quality',
+            values: []
+        }]
+
+        //Fill values
+        for(var i=0; i<250; i++){
+            data[0].values.push({x: ''+i, y: Math.random()});
+        }
+
+        return (
+            <ReactD3.Waveform
+                data={data}
+                width={600}
+                height={200}
+                colorScale={ d3.scale.linear()
+                    .domain([0,1400])
+                    .range(['#eb1785','#ff7b16'])
+                }
+            />         
+        );
+    }    
 });
 
 var DiscoveryGraph = React.createClass({
@@ -114,10 +151,15 @@ var Dashboard = React.createClass({
         });
     },
     render: function(){
-
+        var data = {
+            label: 'Traffic',
+            values: [{x: 'DTN1', y: 68},{x: 'DTN2', y: 27},{x: 'DTN3', y: 10}]
+        };
 
         return(
             <div id="Dash">
+                <TrafficView />
+                <QualityView />
                 <SelectedSourceTable 
                     database={this.state.sourceContext.database} 
                     network={this.state.sourceContext.network} 
@@ -140,4 +182,5 @@ var Dashboard = React.createClass({
 exports.Dashboard = Dashboard;
 exports.DiscoveryGraph = DiscoveryGraph;
 exports.SelectedSourceTable = SelectedSourceTable;
-
+exports.TrafficView = TrafficView;
+exports.QualityView = QualityView;
